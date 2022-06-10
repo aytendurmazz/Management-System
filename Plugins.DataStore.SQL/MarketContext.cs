5f6a9acf93ca087c4cb074feb9805d2159b7cc4c
Ayten Durmaz <ayten.durmaz.99@gmail.com>
2022-06-11 01:17:26 +0300

    Add project files.

---
+++ Plugins.DataStore.SQL/MarketContext.cs
@@ -0,0 +1,26 @@
+﻿using CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+
+namespace Plugins.DataStore.SQL
+{
+    public class MarketContext : DbContext
+    {
+        public MarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>().HasData(
+                    new Product { ProductId = 1, Name = "Iced Tea", Quantity = 100, Price = 1.99 },
+                    new Product { ProductId = 2, Name = "Canada Dry", Quantity = 200, Price = 1.99 },
+                    new Product { ProductId = 3, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50 },
+                    new Product { ProductId = 4, Name = "White Bread", Quantity = 300, Price = 1.50 }
+                );
+        }
+    }
+}
